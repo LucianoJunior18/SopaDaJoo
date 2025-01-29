a3ac51b7ca1737254e65442dabe1a0afc6d4e2d0
junior <junioristylo@gmail.com>
2025-01-29 13:04:45 -0300

    Atualizaçao do site com novos arquivos e modificaçoes

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ document.addEventListener("scroll", ()=>{
         navBar.classList.remove("rolar")
     }
 })
+
+const modal = document.querySelector(".modal")
+const mascara = document.querySelector(".mascara")
+
+function mostrarModal() {
+    modal.style.left = "50%"
+    mascara.style.visibility = "visible"
+}
+
+function esconderModal() {
+    modal.style.left = "-80%"
+    mascara.style.visibility = "hidden"
+
+}
